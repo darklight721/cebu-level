b11cede8a5c0107755a5b4a060875725e64ed65a
Roy Evan Sia <roy.evan.sia@gmail.com>
2023-04-17 17:49:12 +0800

    Add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,155 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import html2canvas from 'html2canvas'
+import { useRef, useState } from 'react'
+import {
+  ListGroup,
+  ListGroupItem,
+  Popover,
+  PopoverBody,
+  PopoverHeader,
+} from 'reactstrap'
 import './App.css'
+import { levels, towns } from './data'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const downloadLink = useRef<HTMLAnchorElement>(null)
+  const [activeTown, setActiveTown] = useState<(typeof towns)[0] | null>(null)
+  const [result, setResult] = useState<Record<string, (typeof levels)[0]>>({})
+  const total = Object.values(result).reduce(
+    (acc, level) => acc + level.points,
+    0
+  )
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="app container-md h-100 position-relative overflow-auto">
+      <div className="links position-absolute top-0 end-0">
+        <a
+          className="main-link d-block text-decoration-none"
+          href="https://github.com"
+        >
+          🏠 https://github.com
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          data-html2canvas-ignore
+          className="ms-1 ms-sm-2 d-block text-decoration-none text-truncate"
+          href="https://my-philippines-travel-level.com/map"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          📣 https://my-philippines-travel-level.com/map
+        </a>
+        <a
+          data-html2canvas-ignore
+          className="ms-1 ms-sm-2 d-block text-decoration-none text-truncate"
+          href="https://zhung.com.tw/japanex"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          📣 https://zhung.com.tw/japanex
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="title-card position-absolute bottom-0 end-0">
+        <h1 className="text-center">
+          Cebu Level <span className="font-monospace">{total}</span>
+        </h1>
+        <div className="bg-white rounded shadow-sm px-2 px-sm-3 py-1 py-sm-2 mt-2 mt-sm-3">
+          {levels.map((level) => (
+            <div key={level.name} className="d-flex align-items-center my-1">
+              <div className={`tile rounded-1 ${level.className}`} />
+              <div className="flex-fill px-2">{level.name}</div>
+              <div>
+                Level:
+                <span className="ms-2 font-monospace">{level.points}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="text-center mt-3">
+          <a
+            data-html2canvas-ignore
+            ref={downloadLink}
+            className="btn btn-primary"
+            download="cebulevel.png"
+            onClick={(e) => {
+              const target = e.target as HTMLAnchorElement
+              if (target.href) return
+
+              e.preventDefault()
+              html2canvas(document.body).then((canvas) => {
+                target.setAttribute('href', canvas.toDataURL())
+                target.click()
+              })
+            }}
+          >
+            Save Image
+          </a>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <svg
+        className="w-100 h-100"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 191.769 373.276"
+      >
+        {towns.map((town) =>
+          town.paths.length > 1 ? (
+            <g
+              key={town.id}
+              id={town.id}
+              className={`town ${result[town.id]?.className ?? ''}`}
+              onClick={() => setActiveTown(town)}
+              {...(town.props ?? {})}
+            >
+              <title>{town.name}</title>
+              {town.paths.map((path) => (
+                <path key={path} d={path} />
+              ))}
+            </g>
+          ) : (
+            <path
+              key={town.id}
+              id={town.id}
+              className={`town ${result[town.id]?.className ?? ''}`}
+              d={town.paths[0]}
+              onClick={() => setActiveTown(town)}
+            >
+              <title>{town.name}</title>
+            </path>
+          )
+        )}
+      </svg>
+      {activeTown && (
+        <Popover
+          hideArrow
+          isOpen
+          toggle={() => setActiveTown(null)}
+          key={activeTown.id}
+          target={activeTown.id}
+          trigger="legacy"
+          className="details-popover"
+        >
+          <PopoverHeader>{activeTown.name}</PopoverHeader>
+          <PopoverBody className="p-0">
+            <ListGroup flush>
+              {levels.map((level) => (
+                <ListGroupItem
+                  key={level.name}
+                  action
+                  tag="button"
+                  active={result[activeTown.id]?.className === level.className}
+                  className={`level-choice ${level.className}`}
+                  onClick={() => {
+                    setResult((prev) => ({ ...prev, [activeTown.id]: level }))
+                    setActiveTown(null)
+                    if (downloadLink.current?.href)
+                      downloadLink.current.removeAttribute('href')
+                  }}
+                >
+                  {level.name}
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </PopoverBody>
+        </Popover>
+      )}
     </div>
   )
 }
-
-export default App
